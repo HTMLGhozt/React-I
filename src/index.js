526c35ddfa58122068339fcb2584dd9f4d8cee22
Gary Dillard <thomas.dillard.studios@gmail.com>
2017-10-24 00:03:57 -0700

    updated CSS: color after check, unique keys, fixed check issue

--- src/index.js
+++ src/index.js
@@ -2,63 +2,73 @@
 /*eslint linebreak-style: ["error", "windows"]*/
 import React, {Component} from 'react';
 import { render } from 'react-dom';
-//import './index.css';
-// You'll want to import the necessary components you want the App component to render
+
 class ToDo extends Component {
   constructor () {
     super();
     this.state = {
       ToDoList: [],
+      ListItem: '',
     };
+    this.count = 0
     this.handleToDoList = this.handleToDoList.bind(this);
     this.handleAddItemToList = this.handleAddItemToList.bind(this);
-    this.handleRemoveItem = this.handleRemoveItem.bind(this);  
+    this.handleRemoveItem = this.handleRemoveItem.bind(this); 
   }
-
   handleToDoList(event) {
-    this.setState({ ListItem: event.target.value });
-  }
-  render() {
-    //console.log(JSON.stringify(this.state.ToDoList));
-    return (
-      <List
-        handleToDoList = {this.handleToDoList}
-        ListItem = {this.state.ListItem}
-        handleAddItemToList = {this.handleAddItemToList}
-        handleRemoveItem = {this.handleRemoveItem}
-        ToDoList = {this.state.ToDoList}
-      />
-    );
+    this.setState({ ListItem: event.target.value, });
   }
   handleAddItemToList() {
     this.state.ToDoList.push({ text: this.state.ListItem, completed: false, });
-    this.setState({ ToDoList: this.state.ToDoList, ListItem: ''});    
+    this.setState({ ToDoList: this.state.ToDoList, ListItem: '', });    
   }
   handleRemoveItem() {
-    let test = this.state.ToDoList.filter(value => value.completed !== true);
-    this.setState({ ToDoList: test, ListItem: ''});
+    this.count++;
+    let ToDoList = this.state.ToDoList.filter(value => value.completed !== true);
+    this.setState({ ToDoList, ListItem: ''});
+  }
+
+  render() {
+    return (
+      <List
+        count = {this.count}
+        ToDoList = {this.state.ToDoList}
+        ListItem = {this.state.ListItem}
+        handleToDoList = {this.handleToDoList}
+        addItemToList = {this.handleAddItemToList}
+        removeItem = {this.handleRemoveItem}
+      />
+    );
   }
 }
 
 const List = props => {
   return (
     <div>
-      <input type='text' value={props.ListItem} onChange={props.handleToDoList} />
-      <button onClick={props.handleAddItemToList}>Submit Item</button>
-      <ul>{
-        props.ToDoList.map((foofie, i) => {
-          return <li key={i+1}>
-            <input key={i+1} 
-              type="checkbox" 
+      <span>
+        <input 
+          type='text' 
+          value={props.ListItem} 
+          onChange={props.handleToDoList} 
+        />
+        <button onClick={props.addItemToList}>Submit Item</button>
+      </span>
+
+      { props.ToDoList.map((foofie, i) => {
+        return (
+          <label className="listItem" key={`label_${props.count}${i}`}>
+            <input 
+              key={`fakeSpan_${props.count}${i}`}
+              type='checkbox'
               onClick={() => {
-                props.ToDoList[i].completed = true; 
-                props.handleRemoveItem();
+                props.ToDoList[i].completed = true;
+                setTimeout(() => {props.removeItem()}, 5000);
               }}
             />
-            {foofie.text}
-          </li>;
-        })
-      }</ul>
+            <span key={`span_${props.count}${i}`}>{foofie.text}</span>
+          </label>
+        );
+      }) }
     </div>
   );
 };
