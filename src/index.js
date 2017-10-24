a6ae6ad3f36327d690e2cb439bc8958e3f13baf1
Gary Dillard <thomas.dillard.studios@gmail.com>
2017-10-23 18:13:26 -0700

    delete installed

--- src/index.js
+++ src/index.js
@@ -1,54 +1,66 @@
 /*eslint no-unused-vars: "off"*/
+/*eslint linebreak-style: ["error", "windows"]*/
 import React, {Component} from 'react';
 import { render } from 'react-dom';
+//import './index.css';
 // You'll want to import the necessary components you want the App component to render
 class ToDo extends Component {
-    constructor () {
-        super();
-        this.state = {
-            ToDoList: [],
-        };
-        this.handleToDoList = this.handleToDoList.bind(this);
-        this.handleAddItemToList = this.handleAddItemToList.bind(this);
-        
-    }
+  constructor () {
+    super();
+    this.state = {
+      ToDoList: [],
+    };
+    this.handleToDoList = this.handleToDoList.bind(this);
+    this.handleAddItemToList = this.handleAddItemToList.bind(this);
+    this.handleRemoveItem = this.handleRemoveItem.bind(this);  
+  }
 
-handleToDoList(event) {
+  handleToDoList(event) {
     this.setState({ ListItem: event.target.value });
-}
-render() {
-	console.log(this.state)
+  }
+  render() {
+    //console.log(JSON.stringify(this.state.ToDoList));
     return (
       <List
-       handleToDoList = {this.handleToDoList}
-       ListItem = {this.state.ListItem}
-       handleAddItemToList = {this.handleAddItemToList}
-       ToDoList = {this.state.ToDoList}
+        handleToDoList = {this.handleToDoList}
+        ListItem = {this.state.ListItem}
+        handleAddItemToList = {this.handleAddItemToList}
+        handleRemoveItem = {this.handleRemoveItem}
+        ToDoList = {this.state.ToDoList}
       />
-    )
-};
-handleAddItemToList() {
-    this.state.ToDoList.push(this.state.ListItem);
+    );
+  }
+  handleAddItemToList() {
+    this.state.ToDoList.push({ text: this.state.ListItem, completed: false, });
     this.setState({ ToDoList: this.state.ToDoList, ListItem: ''});    
+  }
+  handleRemoveItem() {
+    let test = this.state.ToDoList.filter(value => value.completed !== true);
+    this.setState({ ToDoList: test, ListItem: ''});
+  }
 }
-}
-const App = () => (
-   /* Replace this div with whatever components you want the App root component to render */
-   <div>{new ToDo()}</div> 
-);
 
 const List = props => {
-    return (
-        <div>
-            <input type='text' value={props.ListItem} onChange={props.handleToDoList} />
-            <button onClick={props.handleAddItemToList}>Submit Item</button>
-            <ul>
-                {props.ToDoList.map((foofie, i) => {
-                    return <li key={i+1}><input key={i+1} type="checkbox" />{foofie}</li>;
-                })}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <input type='text' value={props.ListItem} onChange={props.handleToDoList} />
+      <button onClick={props.handleAddItemToList}>Submit Item</button>
+      <ul>{
+        props.ToDoList.map((foofie, i) => {
+          return <li key={i+1}>
+            <input key={i+1} 
+              type="checkbox" 
+              onClick={() => {
+                props.ToDoList[i].completed = true; 
+                props.handleRemoveItem();
+              }}
+            />
+            {foofie.text}
+          </li>;
+        })
+      }</ul>
+    </div>
+  );
 };
 
 render(<ToDo />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -17,9 +17,10 @@ module.exports = {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       {
-        test: /\.scss$/,
-        loaders: ["style-loader", "css-loader", "sass-loader"]
-      }
+        test: /\.css$/,
+        loaders: ["style-loader", "css-loader"],
+        exclude: ['node_modules']
+      }   
     ],
   },
   plugins: [HtmlWebpackPluginConfig],
